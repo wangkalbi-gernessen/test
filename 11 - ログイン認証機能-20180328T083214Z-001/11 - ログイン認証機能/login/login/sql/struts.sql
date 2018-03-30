3b35da5b62e8ff7a8a924957606a00a1fb90de27
Kazunobu Someya <Angriff@Kazunobu-no-MacBook-Pro.local>
2018-03-30 10:18:40 +0900

    march30

---
+++ 11 - ログイン認証機能-20180328T083214Z-001/11 - ログイン認証機能/login/login/sql/struts.sql
@@ -0,0 +1,18 @@
+SET names utf8;
+SET foreign_key_checks = 0;
+
+drop database if exists logindb;
+create database logindb;
+use logindb;
+
+create table user(
+id int,
+user_name varchar(255),
+password varchar(255)
+);
+
+insert into user values
+(1,"taro","123"),
+(2,"jiro","123"),
+(3,"hanako","123"),
+(4,"saburo","123");
